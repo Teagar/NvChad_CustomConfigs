35868f2ac1c1a2f1b3533c206bad7f802374fb29
Teagar <cyanofc@gmail.com>
2024-01-23 14:28:51 -0300

    better icons in Mason

--- configs/lsp/mason.lua
+++ configs/lsp/mason.lua
@@ -32,7 +32,7 @@ local settings = {
 		icons = {
 			package_installed = "",
 			package_pending = "",
-			package_uninstalled = "",
+			package_uninstalled = "",
 		},
 	},
 	log_level = vim.log.levels.INFO,
