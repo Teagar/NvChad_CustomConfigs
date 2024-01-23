1bff49c474f48826c34113526b6494994dcb710f
Teagar <cyanofc@gmail.com>
2024-01-23 13:35:23 -0300

    new mason config

--- configs/mason-null-ls.lua
+++ configs/mason-null-ls.lua
@@ -1,6 +1,12 @@
---local status_ok, mason_null_ls = pcall(require, "mason-null-ls")
+local status_ok, mason_null_ls = pcall(require, "mason-null-ls")
 if not status_ok then
 	return
 end
-mason_null_ls.setup({ automatic_setup = true })
-mason_null_ls.setup_handlers({})
+mason_null_ls.setup {
+  ensure_installed = {
+
+  },
+  automatic_setup = true,
+  handlers = {},
+}
+-- mason_null_ls.setup_handlers({})
